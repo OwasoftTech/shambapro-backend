ea74d486be2b165d506c794e60a8484497b14a73
sultan123456 <sultan.ijaz.chaudhary@dynamologic.com>
2022-11-07 21:23:22 +0500

    updated the work

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,16 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\FarmTeam;
-
+use App\Models\Farm;
 use Validator;
 use Carbon\Carbon;
 use Auth;
+
 class AuthController extends Controller
 {
    public function login(Request $request)
    {
-     $validator = Validator::make($request->all(), [
-        'phone_number' => 'required',     
+      $validator = Validator::make($request->all(), [
+         'phone_number' => 'required',
       ]);
 
       if ($validator->fails()) {
@@ -24,67 +25,30 @@ class AuthController extends Controller
       }
 
 
-      $user = User::where('phone_number',$request->phone_number)
-      ->first();
-      if($user){
+      $user = User::where('phone_number', $request->phone_number)
+         ->first();
+      if ($user) {
 
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        $token->expires_at = Carbon::now()->addYear(1);
-        $token->save();
+         $tokenResult = $user->createToken('Personal Access Token');
+         $token = $tokenResult->token;
+         $token->expires_at = Carbon::now()->addYear(1);
+         $token->save();
 
-        return response()->json(['user_id' => $user->id,'token'=>'Bearer ' . $tokenResult->accessToken,'user'=>$user]);
-      }else
-      {
-           return response()->json(['message' => 'User not found','status_code'=> 100]);
-
+         return response()->json(['user_id' => $user->id, 'token' => 'Bearer ' . $tokenResult->accessToken, 'user' => $user]);
+      } else {
+         return response()->json(['message' => 'User not found', 'status_code' => 100]);
       }
-
-
    }
 
 
    public function signup(Request $request)
    {
 
-    $validator = Validator::make($request->all(), [
-        'phone_number' => 'required|unique:users,phone_number',  
-        'name' => 'required',     
-        'farm_name' => 'required',   
-      ]);
-
-      if ($validator->fails()) {
-         $errors = $validator->errors();
-         return response()->json(['error' => $errors->toJson()]);
-      }
-
-
-     $user_id = User::insertGetId([
-        'phone_number'=>$request->phone_number,
-        'name'=>$request->name,
-        'farm_name'=>$request->farm_name,
-        'role'=>$request->role,
-        'email'=>$request->email,
-       ]);
-
-     $user = User::find($user_id);
-     $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        $token->expires_at = Carbon::now()->addYear(1);
-        $token->save();
-
-     return response()->json(['message'=>'Signed up successfully','user_id' => $user_id,'token'=>'Bearer ' . $tokenResult->accessToken]);
-
-       
-   }
-
-
-   public function inviteFriend(Request $request){
-      
       $validator = Validator::make($request->all(), [
-        'phone_number' => 'required|unique:users,phone_number',  
-        'name' => 'required',     
-       ]);
+         'phone_number' => 'required|unique:users,phone_number',
+         'name' => 'required',
+         'farm_name' => 'required',
+      ]);
 
       if ($validator->fails()) {
          $errors = $validator->errors();
@@ -92,33 +56,74 @@ class AuthController extends Controller
       }
 
 
-     $user_id = User::insertGetId([
-        'phone_number'=>$request->phone_number,
-        'name'=>$request->name,
-        'farm_name'=>'',
-        'role'=>$request->role,
-        'email'=>$request->email,
-       ]);
-
-     FarmTeam::insert([
-        'user_id'=>Auth::user()->id,
-        'team_member'=> $user_id,
+      $user_id = User::insertGetId([
+         'phone_number' => $request->phone_number,
+         'name' => $request->name,
+         'farm_name' => $request->farm_name,
+         'role' => $request->role,
+         'email' => $request->email,
       ]);
 
-       return response()->json(['message'=>'Signed Sent Invitaion']);
+      $farm = Farm::where('name', $request->farm_name)->first();
+      if (!$farm) {
+         $farm = Farm::insertGetId([
+            'name' => $request->farm_name,
+         ]);
 
+         $farm = Farm::find($farm);
+      }
+
+      $user = User::find($user_id);
+      $tokenResult = $user->createToken('Personal Access Token');
+      $token = $tokenResult->token;
+      $token->expires_at = Carbon::now()->addYear(1);
+      $token->save();
+
+      return response()->json(['message' => 'Signed up successfully', 'user_id' => $user_id, 'farm' => $farm, 'token' => 'Bearer ' . $tokenResult->accessToken]);
    }
 
 
-   public function myTeam(Request $request){
-      
-     
-      $my_team = FarmTeam::where('user_id',Auth::user()->id)
-      ->with('team')
-     ->get();
+   public function inviteFriend(Request $request)
+   {
+
+      $validator = Validator::make($request->all(), [
+         'phone_number' => 'required|unique:users,phone_number',
+         'name' => 'required',
+      ]);
+
+      if ($validator->fails()) {
+         $errors = $validator->errors();
+         return response()->json(['error' => $errors->toJson()]);
+      }
+
+      $user = Farm::find(Auth::user()->id);
+
+      $user_id = User::insertGetId([
+         'phone_number' => $request->phone_number,
+         'name' => $request->name,
+         'farm_name' => $user->farm_name,
+         'role' => $request->role,
+         'email' => $request->email,
+      ]);
+
+      FarmTeam::insert([
+         'user_id' => Auth::user()->id,
+         'team_member' => $user_id,
+      ]);
+
+      return response()->json(['message' => 'Signed Sent Invitaion']);
+   }
 
 
-       return response()->json(['my_team'=>$my_team]);
+   public function myTeam(Request $request)
+   {
 
+
+      $my_team = FarmTeam::where('user_id', Auth::user()->id)
+         ->with('team')
+         ->get();
+
+
+      return response()->json(['my_team' => $my_team]);
    }
 }
