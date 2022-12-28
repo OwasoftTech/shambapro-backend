80b08484bd00bd2f9a2cca841d3559bb5fe5ac5c
samimughal <38209159+samimughal@users.noreply.github.com>
2022-12-28 12:52:04 +0500

    update routes

--- routes/api.php
+++ routes/api.php
@@ -125,8 +125,8 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/update/livestock/products', [LiveStockProductsController::class, 'update']); 
     Route::post('/delete/livestock/products', [LiveStockProductsController::class, 'delete']);
     Route::get('/detail/livestock/products/{id}', [LiveStockProductsController::class, 'detail']); 
-    Route::post('/add/quantity/livestock/products', [LiveStockProductsController::class, 'add_quantity']); 
-    Route::post('/sell/quantity/livestock/products', [LiveStockProductsController::class, 'remove_quantity']);
+    Route::post('/add/quantity/livestock', [LiveStockProductsController::class, 'add_quantity']); 
+    Route::post('/remove/quantity/livestock', [LiveStockProductsController::class, 'remove_quantity']);
     Route::get('/livestock/history/detail/{id}', [LiveStockProductsController::class, 'livestock_history']);    
 
 
