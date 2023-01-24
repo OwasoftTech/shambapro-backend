ff6b6c89d063caf257d8fcf596cfb35450f5eb0a
samimughal <38209159+samimughal@users.noreply.github.com>
2023-01-24 14:46:25 +0500

    update api

--- app/Http/Controllers/Api/CropManagementRecordController.php
+++ app/Http/Controllers/Api/CropManagementRecordController.php
@@ -404,5 +404,180 @@ class CropManagementRecordController extends Controller
     }    
   }
 
+  public function get_routine_scouting(Request $request)
+  {
+    try 
+    {
+      $routine_scouting =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('routine_scouting',1)
+                      ->select('id','date','sign_symptoms','diagnosis','recommended','batch_no','dosage','supplier_name','expiry_date','status','enterprise_id','user_id',
+                      'created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $routine_scouting]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_weed_management(Request $request)
+  {
+    try 
+    {
+      $weed_management =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('weed_management',1)
+                      ->select('id','date','type','name','ingredient','batch_no','dosage','amount','supplier_name','expiry_date','status',
+                        'enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $weed_management]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_pesticide_application(Request $request)
+  {
+    try 
+    {
+      $pesticide_application =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('pesticide_application',1)
+                      ->select('id','date','method','type','name','ingredient','batch_no','dosage','amount','supplier_name','expiry_date','withholding_days',
+                        'status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $pesticide_application]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_fertilizer_application(Request $request)
+  {
+    try 
+    {
+      $fertilizer_application =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('fertilizer_application',1)
+                      ->select('id','date','method','type','name','ingredient','batch_no','dosage','amount','supplier_name','expiry_date',
+                        'status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $fertilizer_application]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_manure_application(Request $request)
+  {
+    try 
+    {
+      $manure_application =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('manure_application',1)
+                      ->select('id','date','method','type','dosage','amount','supplier_name',
+                        'status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $manure_application]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_irrigation(Request $request)
+  {
+    try 
+    {
+      $irrigation =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('irrigation',1)
+                      ->select('id','date','type','recommended','status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $irrigation]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_other_farm_activities(Request $request)
+  {
+    try 
+    {
+      $other_farm_activities =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('other_farm_activities',1)
+                      ->select('id','date','purpose','observation','status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $other_farm_activities]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_agronomist_inspection(Request $request)
+  {
+    try 
+    {
+      $agronomist_inspection =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('agronomist_inspection',1)
+                      ->select('id','date','purpose','observation','status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $agronomist_inspection]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_crop_produce_harvested(Request $request)
+  {
+    try 
+    {
+      $crop_produce_harvested =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('crop_produce_harvested',1)
+                      ->select('id','date','quantity','status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $crop_produce_harvested]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
+  public function get_harvest_consumed_workers(Request $request)
+  {
+    try 
+    {
+      $harvest_consumed_workers =  CropManagementRecord::where('user_id',$request->user_id)->where('enterprise_id',$request->enterprise_id)
+                      ->where('harvest_consumed_workers',1)
+                      ->select('id','date','quantity','status','enterprise_id','user_id','created_by','updated_by','created_at','updated_at')
+                      ->get();
+
+      return response()->json(['response' => ['status' => true,'data' => $harvest_consumed_workers]],JsonResponse::HTTP_OK);
+    }  
+    catch (Exception $e) 
+    {
+      return response()->json(['response' => ['status' => false, 'message' => $e->getMessage()]], JsonResponse::HTTP_BAD_REQUEST);
+    }    
+  }
+
 
 }
